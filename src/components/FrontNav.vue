57b6cbf6547b702ce9f392779d1426ef9386864d
Jacky <jacky123789456@gmail.com>
2022-03-14 10:58:47 +0800

    project init

---
+++ src/components/FrontNav.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav class="nav">
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+      <span class="material-icons">
+        reorder
+      </span>
+    </label>
+    <label class="logo">DesignX</label>
+    <ul class="list__group">
+      <li class="list__group__item"><a class="list__group__link" href="#">Home</a></li>
+      <li class="list__group__item"><a class="list__group__link" href="#">About</a></li>
+      <li class="list__group__item"><a class="list__group__link" href="#">Serivces</a></li>
+      <li class="list__group__item"><a class="list__group__link" href="#">Contact</a></li>
+      <li class="list__group__item"><a class="list__group__link" href="#">Feedback</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  .nav {
+    background: #0082e6;
+    height: .8rem;
+    width: 100%;
+    .checkbtn {
+      font-size: .3rem;
+      color: white;
+      float: right;
+      line-height: .8rem;
+      margin-right: .4rem;
+      cursor: pointer;
+      display: none;
+      @media (max-width: 858px) {
+        display: block;
+      }
+    }
+    #check {
+      display: none;
+    }
+    #check:checked ~ ul {
+      left: 0;
+    }
+    .logo {
+      color: white;
+      font-size: .35rem;
+      line-height: .8rem;
+      padding: 0 0 0 1rem;
+      font-weight: bold;
+      @media (max-width: 952px) {
+        font-size: .3rem;
+        padding-left: .5rem;
+      }
+    }
+    .list__group {
+      float: right;
+      margin-right: .2rem;
+      @media (max-width: 858px) {
+        position: fixed;
+        width: 100%;
+        height: calc(100vh - .8rem);
+        background: #2c3e50;
+        top: .8rem;
+        left: -100%;
+        text-align: center;
+        transition: all .5s;
+      }
+      &__item {
+        display: inline-block;
+        line-height: .8rem;
+        margin: 0 .05rem;
+        @media (max-width: 858px) {
+          display: block;
+          margin: .5rem 0;
+          line-height: .3rem;
+        }
+      }
+      &__link {
+        color: white;
+        font-size: .17rem;
+        text-transform: uppercase;
+        padding: .07rem .13rem;
+        border-radius: .25rem;
+        @media (max-width: 952px) {
+          font-size: .16rem;
+        }
+        @media (max-width: 858px) {
+          font-size: .2rem;
+          a.active, a:hover {
+            background: none;
+            color: #0082e6;
+          }
+        }
+      }
+      a.active, a:hover {
+        background: #1b9bff;
+        transition: .5s;
+      }
+    }
+  }
+</style>
---
+++ src/views/Front/FrontView.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h3 class="title">Our Games</h3>
+      <h4 class="subtitle">Amiga games are made for quality entertainment.
+        <br>
+        We strive to be on the top of the crowd's gaming experience.
+      </h4>
+    </div>
+    <div class="card">
+      <a href="#"><img src="" alt=""></a>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .section {
+    padding-top: .48rem;
+  }
+  .title {
+    font-weight: bold;
+    font-size: 0.82rem;
+    line-height: 1.2;
+    letter-spacing: 1;
+    text-align: center;
+    @media (max-width: 768px) {
+      font-size: calc( .32rem + ( .82 - .32 ) * (( 100vw - .24rem ) / (12 - .24)));
+      line-height: calc( 1.4 * (.27rem + (4.6 - 2.26) * (( 100vw - 2rem) / (48 - 20))));
+    }
+  }
+  .subtitle {
+    margin-bottom: .2rem;
+    text-align: center;
+    font-size: .142rem;
+    line-height: 1.6;
+  }
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      data: []
+    }
+  }
+}
+</script>
